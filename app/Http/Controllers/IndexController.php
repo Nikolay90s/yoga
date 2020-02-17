77bea3f78a0b834be9b890240edcd9f8f2c35079
Nikolay <kolarulit@mail.ru>
2020-02-17 18:10:34 +0300

    Добавление шаблона

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function execute() {
+      	if(view()->exists('site.index')) {
+          	$data = [
+            	'title' => 'Yogalax'
+            ];
+          	return view('site.index', $data);
+        } else {
+          	abort(404);
+        }
+    }
 }
