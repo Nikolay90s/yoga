a8d359e71bbe9f2bbcaa7ae1729b42ea039dc472
Nikolay <kolarulit@mail.ru>
2020-02-18 07:50:48 +0300

    Создание навигации

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Menu;
 
 class IndexController extends Controller
 {
     public function execute() {
+
+      	$menus = Menu::all();
+
       	if(view()->exists('site.index')) {
           	$data = [
-            	'title' => 'Yogalax'
+            	'title' => 'Yogalax',
+              	'menus' => $menus
             ];
           	return view('site.index', $data);
         } else {
