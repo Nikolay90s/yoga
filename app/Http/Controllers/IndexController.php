7a8b8cfcdd0c5d2ade799be9599cea2d6cfb21ca
Nikolay <kolarulit@mail.ru>
2020-02-18 22:05:14 +0300

    Программирование слайда

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Menu;
+use App\Slider;
 
 class IndexController extends Controller
 {
     public function execute() {
 
       	$menus = Menu::all();
+      	$sliders = Slider::all()->toArray();
+
+      	$sl = array();
+      	foreach($sliders as $slider){
+          	$text = explode(".", $slider['Vtext']);
+          	$textAll = array();
+          	foreach($text as $tex){
+              	if($tex == " "){
+                  	unset($tex);
+                } else {
+                  	array_push($textAll, $tex);
+                }
+            }
+
+          	$slider["Vtext"] = $textAll;
+          	array_push($sl, $slider);
+        }
 
       	if(view()->exists('site.index')) {
+
           	$data = [
             	'title' => 'Yogalax',
-              	'menus' => $menus
+              	'menus' => $menus,
+              	'sliders' => $sl
             ];
           	return view('site.index', $data);
         } else {
---
+++ database/migrations/2020_02_18_144842_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+          	$table->enum('view', [0,1])->default(0);
+          	$table->string('Vtext', 100);
+          	$table->string('text', 100);
+          	$table->string('buttonName', 100);
+          	$table->string('images', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
