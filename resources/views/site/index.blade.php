76777295081f3b5420bcb35880bdf74e0a1fa79d
Nikolay90s <55440888+Nikolay90s@users.noreply.github.com>
2020-02-18 12:36:06 +0300

    обновление (#7)

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.yoga')
+
+@section('nav')
+    @include('site.includes.nav')
+@endsection
+
+@section('content')
+    @include('site.includes.content')
+@endsection
+
+@section('footer')
+    @include('site.includes.footer')
+@endsection
